f23f07d37fdcd7616d20ecd960e379d6deb32042
Alexys Veloz <alexys.veloz.fl@gmail.com>
2022-08-25 10:46:36 -0400

    updated to be arraylist

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,25 +6,35 @@ import java.nio.file.Paths;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> total = new ArrayList<>();
+        int lines = 0;
 
         try (Scanner in = new Scanner(Paths.get("in.txt"))) {
             //take in the inputs
             while (in.hasNext()) {
+                lines++;
                 String str = in.nextLine();
                 System.out.println(str);
-                ShiftAndPrint(str);
+                //if you're doing affines, make sure only AffineAndPrint is up
+                //if you're doing shifts, make sure only ShiftAndPrint is up
+                ShiftAndPrint(str, total);
+                //AffineAndPrint(str);
 
             }
         } catch (IOException | NoSuchElementException | IllegalStateException e) {
             e.printStackTrace();
         }
+        //if you're only doing shift ciphers, comment out printAffine
+        // and make sure printShift is up.
+        printShift(total, lines);
+        //if you're doing affine ciphers, only do printAffine
+
     }
-    static void ShiftAndPrint(String str) throws FileNotFoundException {
+    static void ShiftAndPrint(String str, ArrayList totalSt) throws FileNotFoundException {
         char[] strar = str.toCharArray();
         int len = strar.length;
         char[] newstrar = new char[len];
-        String[] Total = new String[26];
         for (int i = 0; i < 26; i++){
             for (int j = 0; j < strar.length; j++){
                 int add = i+1;
@@ -38,17 +48,17 @@ public class Main {
             }
             String newString = String.valueOf(newstrar);
             //System.out.printf("i is %d\n newstrar is %s\n", i, newString);
-            Total[i] = newString;
+            totalSt.add(newString);
             //System.out.printf("and now total[%d] should also be %s\n", i, Total[i]);
         }
-        print(Total);
+
     }
-    static void print(String[] str) throws FileNotFoundException {
+    static void printShift(ArrayList totalSt, int lines) throws FileNotFoundException {
             PrintStream out = new PrintStream(new FileOutputStream("out.txt"));
             System.setOut(out);
 
-            for (int i = 0; i < 26; i++) {
-                System.out.println(str[i]);
+            for (int i = 0; i < lines*26; i++) {
+                System.out.println(totalSt.get(i));
             }
         System.out.printf("\n");
 
